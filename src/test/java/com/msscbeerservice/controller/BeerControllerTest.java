400dadd1a7baf23d08ec3a1bc845211d9f6c40be
rezakar <hamidmick@yahoo.com>
2023-03-09 17:30:19 -0500

    Adding beer controller test. Closes #4

---
+++ src/test/java/com/msscbeerservice/controller/BeerControllerTest.java
@@ -0,0 +1,38 @@
+package com.msscbeerservice.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+@RunWith(SpringRunner.class)
+public class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getBeerById() throws Exception{
+
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+   public void createBeer() {
+    }
+
+    @Test
+   public void updateBeerById() {
+    }
+}
